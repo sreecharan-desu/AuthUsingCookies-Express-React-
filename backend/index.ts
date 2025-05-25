00fb03fc39ffbe888a300f86d17deebf2370b4e6
SreeCharan Desu <sreecharan309@gmail.com>
2025-05-25 19:05:26 +0530

    added/ build command for backend

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://auth-using-cookies-express-react.vercel.app/",
     credentials : true
 }))
 
